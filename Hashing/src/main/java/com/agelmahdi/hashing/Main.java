53a443ba9f6118f3040ac337e350a8c38ddd9c82
Ahmed El-Mahdi <agelmahdi@gmail.com>
2021-09-09 17:31:12 +0200

    [Open addressing][Linear Probing] loadFactor, reHashing, insertion

--- Hashing/src/main/java/com/agelmahdi/hashing/Main.java
+++ Hashing/src/main/java/com/agelmahdi/hashing/Main.java
@@ -1,14 +1,19 @@
 package com.agelmahdi.hashing;
 
+import com.agelmahdi.hashing.OpenAddressing.LinearProbing;
+
 public class Main {
 
     public static void main(String[] args) {
-        DirectChaining directChaining = new DirectChaining(1);
-        directChaining.insertHashTable("Ahmed");
-        directChaining.insertHashTable("gamal");
-        directChaining.insertHashTable("mahdi");
+        LinearProbing directChaining = new LinearProbing(3);
+        directChaining.insert("a");
+        directChaining.insert("b");
+        directChaining.insert("c");
+        directChaining.insert("d");
+        directChaining.insert("e");
+        directChaining.insert("f");
+        directChaining.insert("g");
 
-        directChaining.deleteKeyHashTable("gamal");
         directChaining.DisplayHashTable();
     }
 
