9f62392f2b04ab86aa8cbfbb4ece3ce6a61bda7d
Ahmed El-Mahdi <agelmahdi@gmail.com>
2021-09-14 20:58:34 +0200

    [Binary Search] Half of the remaining elements can be eliminated at a time, instead of eliminating one be one

--- SearchingAlgorithms/src/main/java/com/agelmahdi/searchingalgorithms/BinarySearch.java
+++ SearchingAlgorithms/src/main/java/com/agelmahdi/searchingalgorithms/BinarySearch.java
@@ -5,6 +5,16 @@ public class BinarySearch {
     // Time complexity O(log(n))
     // Space complexity O(1)
 
+
+    /*
+    * log2(n) = 2~x = n
+    * -> 16 elements = 4 Steps
+    * -> log2(16) = 2~x = 2~4 = 16 = 2*2*2*2 = 16
+    *
+    * -> 32 elements = 5 Steps
+    * -> log2(32) = 2~x = 2~5 = 32 = 2*2*2*2*2 = 32
+    * */
+
     public int search(int[] arr, int value) {
         int left = 0;
         int right = arr.length - 1;
