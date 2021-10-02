8c3515c3d93998e8b11423c535dfbde7ea3720dc
Ahmed El-Mahdi <agelmahdi@gmail.com>
2021-10-02 22:20:53 +0200

    [Top down with Memoization] this technique of sorting the results of already solved sub-problems

---
+++ DynamicProgramming/src/main/java/com/agelmahdi/dynamicprogramming/Main.java
@@ -0,0 +1,8 @@
+package com.agelmahdi.dynamicprogramming;
+
+public class Main {
+    public static void main(String[] args) {
+        FibonacciMemoization fibMemo = new FibonacciMemoization();
+        System.out.println(fibMemo.fib(6));
+    }
+}
