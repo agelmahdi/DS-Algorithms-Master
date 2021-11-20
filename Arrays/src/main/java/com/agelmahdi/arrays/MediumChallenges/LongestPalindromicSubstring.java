f67e7b7d6e11e6b1115ec492edffae1e61c4a4cf
Ahmed El-Mahdi <agelmahdi@gmail.com>
2021-11-20 22:39:41 +0200

    [Arrays][Medium challenge] longest palindrom sub-string

--- Arrays/src/main/java/com/agelmahdi/arrays/MediumChallenges/LongestPalindromicSubstring.java
+++ Arrays/src/main/java/com/agelmahdi/arrays/MediumChallenges/LongestPalindromicSubstring.java
@@ -1,4 +1,97 @@
 package com.agelmahdi.arrays.MediumChallenges;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestPalindromicSubstring {
+
+    // Expand Around Center
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+    public static String longestPalindrome2(String s) {
+
+        if (s.length() == 0) return "";
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        int max = Integer.MIN_VALUE;
+
+        int j = 0;
+
+        String str = "";
+
+        String sub = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+
+            if (str.contains(String.valueOf(s.charAt(i)))) {
+
+                if (s.charAt(i) == s.charAt(j)){
+                    j = str.indexOf(s.charAt(i)) ;
+                }
+                else {
+                    j = str.lastIndexOf(s.charAt(i)) ;
+
+                }
+
+            }
+
+            sub = s.substring(j, i + 1);
+
+            if (isPalindrome(sub)){
+                map.put(sub.length(), sub);
+            }
+            str += String.valueOf(s.charAt(i));
+
+
+        }
+
+        for (Map.Entry<Integer, String> entry: map.entrySet()){
+            max = Math.max(max, entry.getKey());
+        }
+
+
+      // System.out.println(Arrays.toString(map.entrySet().toArray()));
+        return map.get(max);
+    }
+
+    public static boolean isPalindrome(String s) {
+
+        for (int i = 0; i < s.length() / 2; i++) {
+
+            int lastIndex = s.length() - i - 1;
+
+            if (s.charAt(i) == s.charAt(lastIndex)) {
+                continue;
+            }
+
+            return false;
+        }
+       //  System.out.println("Plindrom: "+ s);
+
+        return true;
+    }
 }
--- Arrays/src/main/java/com/agelmahdi/arrays/MediumChallenges/Main.java
+++ Arrays/src/main/java/com/agelmahdi/arrays/MediumChallenges/Main.java
@@ -17,20 +17,36 @@ public class Main {
         int [][]matrix3 = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
        // SetMatrixZeroes.setZeroes(matrix);
 
-        String []strs = {"eat","tea","tan","ate","nat","bat"};
-        String []strs2 = {"a"};
-        String []strs3 = {""};
+//        String []strs = {"eat","tea","tan","ate","nat","bat"};
+//        String []strs2 = {"a"};
+//        String []strs3 = {""};
+//
+//        System.out.println(Arrays.toString(new List[]{GroupAnagrams.groupAnagrams(strs3)}));
+//
+//        GroupAnagrams.groupAnagrams(strs);
+//
+//        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "abcabcbb"));
+//        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "bbbbb"));
+//        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "pwwkew"));
+//        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( " "));
+//        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "au"));
+//        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "abcb"));
+//        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "dvdf")); // 3
 
-       // System.out.println(Arrays.toString(new List[]{GroupAnagrams.groupAnagrams(strs3)}));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("babad"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("abcdcba"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("cbbd"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("a"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("ac"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("bb"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("abcda"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("aa"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("aca"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("bacabab"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("aacabdkacaa"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("ccccd"));
+        System.out.println(LongestPalindromicSubstring.longestPalindrome("xaabacxcabaax")); //  xaabacxcabaax
 
-      //  GroupAnagrams.groupAnagrams(strs);
 
-        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "abcabcbb"));
-        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "bbbbb"));
-        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "pwwkew"));
-        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( " "));
-        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "au"));
-        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "abcb"));
-        System.out.println(LongestSubstringWithoutDups.lengthOfLongestSubstring( "dvdf")); // 3
     }
 }
