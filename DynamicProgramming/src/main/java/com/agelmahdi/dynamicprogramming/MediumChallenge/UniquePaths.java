f3adc3152a4553546fa766a92c11dfc178755ba3
Ahmed El-Mahdi <agelmahdi@gmail.com>
2021-12-19 21:06:40 +0200

    [Dynamic programming][Medium challenge] find how many unique paths

--- DynamicProgramming/src/main/java/com/agelmahdi/dynamicprogramming/MediumChallenge/UniquePaths.java
+++ DynamicProgramming/src/main/java/com/agelmahdi/dynamicprogramming/MediumChallenge/UniquePaths.java
@@ -10,6 +10,21 @@ class UniquePaths {
         uniquePathsMathSolution(3,7);
     }
 
+    public static int uniquePaths(int m, int n) {
+        int[][] tabular = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            tabular[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            tabular[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                tabular[i][j] = tabular[i - 1][j] + tabular[i][j - 1];
+            }
+        }
+        return tabular[m - 1][n - 1];
+    }
 
     public static int uniquePathsMathSolution(int m, int n) {
         if (m == 1 || n == 1)
